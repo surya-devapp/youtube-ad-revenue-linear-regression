0ca5323a59a0c11c6031e49a986321d4c2511068
surya-devapp <surya.sff3655@gmail.com>
2025-12-06 23:52:21 +0530

    removed unwanted file

--- check_data_values.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("youtube_ad_revenue_dataset.csv")
-print(df[["views", "watch_time_minutes", "video_length_minutes", "likes", "comments"]].head())
-print("\nStats:")
-print(df[["views", "watch_time_minutes", "video_length_minutes"]].describe())
